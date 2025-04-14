1b0da852cd39fe29e4f567375e302b2923895f02
Abdussalam <abdussalamabdulmojeed@gmail.com>
2025-04-14 10:58:01 +0100

    complete dice joke roller

---
+++ script.js
@@ -0,0 +1,18 @@
+const id = document.getElementById('id');
+const quote = document.getElementById('quote');
+
+async function quotes() {
+    try {
+        const request = await fetch('https://api.adviceslip.com/advice');
+    const response = await request.json();
+     id.textContent = response.slip.id;
+     quote.textContent = response.slip.advice;
+    console.log(response);
+    } catch (error) {
+        console.log("error message is", error);
+};
+};
+function roll(){
+    
+quotes();
+}
